36cfba61909ce3677e0cc352947fdfc18d6e230f
thermatk <thermatk@thermatk.com>
2022-10-02 19:05:05 +0200

    [TF][KILL] Google Login

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -1382,7 +1382,6 @@ public class MessagesController extends BaseController implements NotificationCe
         storiesPosting = mainPreferences.getString("storiesPosting", "enabled");
         storiesEntities = mainPreferences.getString("storiesEntities", "premium");
         storiesExportNopublicLink = mainPreferences.getBoolean("storiesExportNopublicLink", false);
-        BuildVars.GOOGLE_AUTH_CLIENT_ID = mainPreferences.getString("googleAuthClientId", BuildVars.GOOGLE_AUTH_CLIENT_ID);
         if (mainPreferences.contains("dcDomainName2")) {
             dcDomainName = mainPreferences.getString("dcDomainName2", "apv3.stel.com");
         } else {
@@ -2243,11 +2242,11 @@ public class MessagesController extends BaseController implements NotificationCe
                 case "login_google_oauth_client_id": {
                     if (value.value instanceof TLRPC.TL_jsonString) {
                         String str = ((TLRPC.TL_jsonString) value.value).value;
-                        if (!Objects.equals(BuildVars.GOOGLE_AUTH_CLIENT_ID, str)) {
+                        /*if (!Objects.equals(BuildVars.GOOGLE_AUTH_CLIENT_ID, str)) {
                             BuildVars.GOOGLE_AUTH_CLIENT_ID = str;
                             editor.putString("googleAuthClientId", BuildVars.GOOGLE_AUTH_CLIENT_ID);
                             changed = true;
-                        }
+                        }*/
                     }
                     break;
                 }
